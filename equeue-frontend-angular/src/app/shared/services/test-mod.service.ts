6ac813582f70bfdf4556963a66940314c47b3364
wj <simweijie1@gmail.com>
2021-04-02 15:39:23 +0800

    Removed Allow-Access headers in service.ts and enabled CORS in API GW to fix CORS bug

--- equeue-frontend-angular/src/app/shared/services/test-mod.service.ts
+++ equeue-frontend-angular/src/app/shared/services/test-mod.service.ts
@@ -12,10 +12,6 @@ type EntityArrayResponseType = HttpResponse<ITestMod[]>;
 export class TestModService {
   httpOptions: HttpHeaders = new HttpHeaders({
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': 'true',
-      'Access-Control-Allow-Headers': 'Content-Type',
-      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
       'x-api-key': 'Pbo1e5P62c9YPYYbmyT8a703Fst9Eb4YaGipoMRd'
   });
 
