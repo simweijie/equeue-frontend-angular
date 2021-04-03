4b52399c148a3cd2a2a9b55a446ad7ac50e1eef7
Chin Sok Fang <sokfang.chin@ncs.com.sg>
2021-04-03 11:51:42 +0800

    side-bar menu

--- equeue-frontend-angular/src/app/main/main.component.ts
+++ equeue-frontend-angular/src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+// import { openNav, closeNav } from '../shared/lalla.js';
 
 @Component({
     selector: 'ic-main',
@@ -8,4 +9,14 @@ import { Component, OnInit, HostListener } from '@angular/core';
 export class MainComponent implements OnInit {
 
     ngOnInit() {}
-}
+
+
+     openNav() {
+        Document.getElementById("myNav").style.width = "100%";
+    }
+    
+    /* Close when someone clicks on the "x" symbol inside the overlay */
+    closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+    }
+  
